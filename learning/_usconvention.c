77102c9e69d70d3d54544ff8ddb35d76fba5f445
asimetra404 <asimetra404@gmail.com>
2025-01-30 14:56:39 +0300

    Refactor argument parsing for clarity and consistency; improve formatting

---
+++ learning/_usconvention.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int arr<:5:> = <% 5, 4, 3, 2, 1 %>;
+    if (1<:arr:> == 5 && ((printf("true-case\n"), 1)))
+    <% 
+        goto end;
+    %>
+    printf("false-case\n");
+    end:
+    int i = 5;
+    printf("%d\n", -~i);
+}
--- src/parser/args_parser.c
+++ src/parser/args_parser.c
@@ -6,39 +6,39 @@
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:14:21 by hsamir            #+#    #+#             */
-/*   Updated: 2025/01/29 17:54:53 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/01/30 14:11:41 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
 
-int parse_uint(char* str, int* err)
+int	parse_uint(char *str, int *err)
 {
-    long	number;
+	long	number;
 
 	if (!is_valid_unumber(str))
-    {
-        *err = 1;
-        return (0);
-    }
+	{
+		*err = 1;
+		return (0);
+	}
 	number = strtoui(str, err);
 	if (*err)
 		return (0);
 	return (number);
 }
 
-t_sim_config    parse_arguments(int argc, char** argv)
-{        
-    t_sim_config config;
-    
-    config.max_meals = -1;
-    config.err = 0; 
-    config.max_meals = -1;
-    config.philo_count = parse_uint(argv[1], &config.err);
-    config.die_time_ms = parse_uint(argv[2], &config.err);
-    config.eat_time_ms = parse_uint(argv[3], &config.err);
-    config.sleep_time_ms = parse_uint(argv[4], &config.err);
-    if (argc == 6)
-        config.max_meals = parse_uint(argv[5], &config.err); 
-    return (config);
+t_sim_config	parse_arguments(int argc, char **argv)
+{
+	t_sim_config	config;
+
+	config.max_meals = -1;
+	config.err = 0;
+	config.max_meals = -1;
+	config.philo_count = parse_uint(argv[1], &config.err);
+	config.die_time_ms = parse_uint(argv[2], &config.err);
+	config.eat_time_ms = parse_uint(argv[3], &config.err);
+	config.sleep_time_ms = parse_uint(argv[4], &config.err);
+	if (argc == 6)
+		config.max_meals = parse_uint(argv[5], &config.err);
+	return (config);
 }
