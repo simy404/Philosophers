66f1f3a342bb639cc87855b88a49eb848d48c993
asimetra404 <asimetra404@gmail.com>
2025-02-06 11:21:35 +0300

    Fix error handling in simulation initialization and start; remove unnecessary return statements

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:50:35 by hsamir            #+#    #+#             */
-/*   Updated: 2025/02/06 08:54:39 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/02/06 11:18:11 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	main(int argc, char **argv)
 		return (printf(ARGUMENT_ERROR));
 	simulation = initialize_simulation(config);
 	if (!simulation)
-		return (printf("Error: Simulation initialization failed\n")); //TODO I have to check if condition is correct, i ensure that free everything
+		return (printf("Error: Simulation initialization failed\n"));
 	printf("<philos:%d> <t_die:%d> <t_eat:%d> <t_sleep:%d> [eat_count:%d], err check:%d\n", simulation->philo_count,config.die_time_ms,config.eat_time_ms,config.sleep_time_ms,config.max_meals,config.err);
 	if (!start_simulation(simulation))
-		return (printf("Error: Simulation failed\n")); // Same check -> line 28
+		printf("Error: Simulation failed\n");
 	abort_simulation(simulation);
 }
